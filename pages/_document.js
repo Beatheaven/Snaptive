34b2c733f86130b000e31349fd47c47b022aaf96
Rifansyah Aditya Mahendra <75738360+Beatheaven@users.noreply.github.com>
2023-10-02 23:34:51 +0700

    Update _document.js
    add google search console

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
       <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
+      <meta name="google-site-verification" content="EwQnbtgsaQYxfiFS8pB6fZuSkeBVLO2wQcNqd6ASpu0" />
       </Head>
       <body>
         <Main />
